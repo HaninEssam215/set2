8c8fb7aba8d1460634585b40d5010d0e54151e96
Hanin Essam <he106400@gmail.com>
2023-07-22 02:00:18 +0300

    Font Awesome icons worked

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
 import weasyprint
-import base64
-from weasyprint.text.fonts import FontConfiguration
+# import base64
+# from weasyprint.text.fonts import FontConfiguration
 
 
-path_to_file = 'reminder.html'
+# path_to_file = 'reminder.html'
 
-# Read the HTML content from the file
-with open(path_to_file, 'r', encoding='utf-8') as file:
-    html_content = file.read()
+# # Read the HTML content from the file
+# with open(path_to_file, 'r', encoding='utf-8') as file:
+#     html_content = file.read()
 
-# Create a WeasyPrint CSS object and customize the options
-font_config = FontConfiguration()
+# # Create a WeasyPrint CSS object and customize the options
+# font_config = FontConfiguration()
 css = weasyprint.CSS(string="""
-@font-face {
-    font-family: 'Forum';
-    src: url('fonts/Forum-Regular.ttf') format('truetype');
-}
 @page {
     margin: 0;
     padding: 0;
@@ -25,11 +21,25 @@ body {
     padding: 0;
     font-family: 'Forum', cursive;
 }
-""", font_config=font_config)
+""")
 
 
 
-# Generate the PDF using WeasyPrint and apply the custom options
+# # Generate the PDF using WeasyPrint and apply the custom options
+# pdf = weasyprint.HTML(string=html_content).write_pdf(stylesheets=[css], encoding='utf-8')
+
+# # Save the PDF to a file with the correct encoding
+# with open('output.pdf', 'wb') as file:
+#     file.write(pdf)
+
+
+path_to_file = 'reminder.html'
+
+# Read the HTML content from the file
+with open(path_to_file, 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+# Generate the PDF using WeasyPrint
 pdf = weasyprint.HTML(string=html_content).write_pdf(stylesheets=[css], encoding='utf-8')
 
 # Save the PDF to a file with the correct encoding
